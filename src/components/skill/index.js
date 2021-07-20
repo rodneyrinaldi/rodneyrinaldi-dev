1fa0336bd5d19a5c65dc152ab027a2ed598672da
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-07-20 06:14:07 -0300

    update project v2

--- src/components/skill/index.js
+++ src/components/skill/index.js
@@ -1,15 +1,12 @@
-import Link from 'next/link'
 import React from 'react';
 
 import styles from './index.module.css';
 
 function Skill({ url, alt, pic }) {
   return (
-    <Link href={url}>
-      <a href={url} className={styles.card}>
-        <img src={pic} alt={alt} className={styles.logo} />
-      </a>
-    </Link>
+    <a className={styles.card}>
+      <img src={pic} alt={alt} className={styles.logo} />
+    </a>
   );
 }
 
