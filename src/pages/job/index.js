d8271d7065ad48f2916ed4c5eb11f20e055ada3d
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-07-23 14:44:12 -0300

    update project v2

--- src/pages/job/index.js
+++ src/pages/job/index.js
@@ -11,17 +11,18 @@ function Job() {
           <div className={styles.row}>
             <p>Olá,</p>
             <p>
-              Meu nome é Rodney e fui desenvolvedor sênior client/server na plataforma Microsoft.
-              Estou atualizado codando no ecossistema Javascript/Node/React.
+              Meu nome é Rodney, fui desenvolvedor client / server na plataforma Microsoft durante os anos 90 e 2000.
+              Me atualizei e estou codando no ecossistema Javascript / Node / React.
               Também fui dba Oracle, experiente em SqlServer e atualizado nos bancos Postgres/Mongo/Redis.
             </p>
             <p>
               De volta ao desenvolvimento procuro uma oportunidade que me permita pegar o rítimo novamente.
-              Entendo que possa haver motivos que tornem esta situação interessante para ambos.
+              Entendo que possa haver motivos que tornem esta situação interessante para todos.
             </p>
             <br />
             <p>Fico à disposição para contatos, obrigado.</p>
             <p>Rodney Rinaldi</p>
+
             <Link href='/portfolio'>
               <a href='/portfolio'>
                 http://dev.rodneyrinaldi.com/portfolio
