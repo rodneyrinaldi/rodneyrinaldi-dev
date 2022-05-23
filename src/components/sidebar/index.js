a1378f157b0ba095751c37c8f287ac7a7cabc917
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2022-05-23 18:59:54 -0300

    addiing internalization scheme

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -11,33 +11,26 @@ function Contact(props) {
     <>
       <div className={styles.wrapperTop}>
         <a onClick={() => router.push("/")}>
-          <Image
-            src="/sidebar/home.png"
-            alt="email"
-            className={styles.icon}
-            width="42"
-            height="42"
-          />
+          <div className={styles.icon}>
+            <Image src="/sidebar/home.png" alt="email" width="42" height="42" />
+          </div>
         </a>
         <br />
         <a href="#toppage">
-          <Image
-            src="/sidebar/top.png"
-            alt="go top"
-            className={styles.icon}
-            width="42"
-            height="42"
-          />
+          <div className={styles.icon}>
+            <Image src="/sidebar/top.png" alt="go top" width="42" height="42" />
+          </div>
         </a>
         {props.showback === "yes" ? (
           <a onClick={() => router.back()}>
-            <Image
-              src="/sidebar/back.png"
-              alt="go back"
-              className={styles.icon}
-              width="42"
-              height="42"
-            />
+            <div className={styles.icon}>
+              <Image
+                src="/sidebar/back.png"
+                alt="go back"
+                width="42"
+                height="42"
+              />
+            </div>
           </a>
         ) : (
           <></>
@@ -46,24 +39,26 @@ function Contact(props) {
 
       <div className={styles.wrapperBottom}>
         <a href={wapp} target="_blank" rel="noreferrer">
-          <Image
-            src="/sidebar/whatsapp.png"
-            alt="whatsapp"
-            className={styles.icon}
-            target="_blank"
-            rel="noreferrer"
-            width="42"
-            height="42"
-          />
+          <div className={styles.icon}>
+            <Image
+              src="/sidebar/whatsapp.png"
+              alt="whatsapp"
+              target="_blank"
+              rel="noreferrer"
+              width="42"
+              height="42"
+            />
+          </div>
         </a>
         <a onClick={() => router.push("/message")}>
-          <Image
-            src="/sidebar/email.png"
-            alt="email"
-            className={styles.icon}
-            width="42"
-            height="42"
-          />
+          <div className={styles.icon}>
+            <Image
+              src="/sidebar/email.png"
+              alt="email"
+              width="42"
+              height="42"
+            />
+          </div>
         </a>
       </div>
     </>
